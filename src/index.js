2d954d03653e4638789cc4ca1c6130627e39075a
Daniel Rodriguez <daniel.r.rodriguez86@gmail.com>
2023-07-18 16:30:01 -0500

    Add distribution folder

--- src/index.js
+++ src/index.js
@@ -1,23 +1,6 @@
 'use strict'
 
 import './styles/index.scss'
-import styles from './styles/styles.module.scss';
-
-console.log("It is really working");
-// console.log(heading);
-
-const obj = {
-  a: 'apple crisp',
-  b: 'buttercream',
-  c: {
-    cd: 'cheese danish'
-  },
-};
-
-console.log(obj?.c?.cd, obj?.f?.fa);
-
-const h5 = document.querySelector('h5');
-h5.className = styles.heading;
 
 if (module.hot) {
   module.hot.accept();
