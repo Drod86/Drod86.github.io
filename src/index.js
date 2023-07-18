0d1e29f186d4991d24c4b6bf5bf3ed5868ebb4eb
Daniel Rodriguez <daniel.r.rodriguez86@gmail.com>
2023-07-18 12:03:26 -0500

    Test JS module transpile

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 'use strict'
 console.log("It is really working");
 
+const obj = {
+  a: 'apple crisp',
+  b: 'buttercream',
+  c: {
+    cd: 'cheese danish'
+  },
+};
+
+console.log(obj?.c?.cd, obj?.f?.fa);
+
 if (module.hot) {
   module.hot.accept();
 }
