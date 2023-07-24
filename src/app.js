9a45c9d0c0a2cc5d3dcaeaa58cacda1c8fc288b1
Daniel Rodriguez <daniel.r.rodriguez86@gmail.com>
2023-07-24 16:28:19 -0500

    Fix eslint errors

--- src/app.js
+++ src/app.js
@@ -1,17 +1,19 @@
-import {grab, onClick, onClickAll, stopListenAll} from './utils/utils.mjs';
+import {
+  grab, onClick, onClickAll, stopListenAll,
+} from './utils/utils.mjs';
 
 const NAV = grab('nav');
 const ITEMS = Object.values(NAV.children);
 const MENU = ITEMS.pop().children[1];
 
-const toggleMenu = () => { 
-  if (NAV.classList.length > 1){
+const toggleMenu = () => {
+  if (NAV.classList.length > 1) {
     stopListenAll(ITEMS, 'click', toggleMenu);
     NAV.classList.remove('open');
   } else {
-    NAV.classList.add('open')
+    NAV.classList.add('open');
     onClickAll(ITEMS, toggleMenu);
-  };
+  }
 };
 
 onClick(MENU, toggleMenu);
