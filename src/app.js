780ba2456cebcf456df7ffa6d37647ad7730fa68
Daniel Rodriguez <daniel.r.rodriguez86@gmail.com>
2023-07-24 15:04:56 -0500

    Add mobile menu styles and functionality

--- src/app.js
+++
@@ -1,4 +0,0 @@
-import {grab, render} from './utils/utils.mjs';
-
-const BODY = grab('body');
-render(BODY, 'testing234');
--- src/utils/utils.mjs
+++ src/utils/utils.mjs
@@ -6,25 +6,32 @@ const grabAll = (selector) => Object.values(document.querySelectorAll(selector))
 const render = (node, markup) => (node.innerHTML = markup);
 
 // listen
-const listen = (node, event, callback, params) => {
-  node.addEventListener(event, (e) => {
-    callback(...params, e);
-  })
+const listen = (node, event, callback) => {
+  node.addEventListener(event, callback)
 } 
-const listenAll = (nodes, event, callback, params) => {
+const listenAll = (nodes, event, callback) => {
   nodes.forEach((node) => {
-    listen(node, event, callback, params);
+    listen(node, event, callback);
+  });
+}
+
+const stopListen = (node, event) => {
+  node.removeEventListener(event)
+} 
+const stopListenAll = (nodes, event) => {
+  nodes.forEach((node) => {
+    stopListen(node, event);
   });
 }
 
 // events
-const onLoad = (node, callback, params) => listen(node, 'load', callback, params);
+const onLoad = (node, callback) => listen(node, 'load', callback);
 
-const onLoadAll = (node, callback, params) => listenAll(node, 'load', callback, params);
+const onLoadAll = (node, callback) => listenAll(node, 'load', callback);
 
-const onClick = (node, callback, params) => listen(node, 'click', callback, params);
+const onClick = (node, callback) => listen(node, 'click', callback);
 
-const onClickAll = (node, callback, params) => listenAll(node, 'click', callback, params);
+const onClickAll = (node, callback) => listenAll(node, 'click', callback);
 
 // Style
 const checkStyle = (el, style) => window.getComputedStyle(el, style);
@@ -33,4 +40,4 @@ const checkStyle = (el, style) => window.getComputedStyle(el, style);
 const OPEN = "open";
 const CLOSE = "close";
 
-export { grab, grabAll, render, onLoad, onLoadAll, onClick, onClickAll, listen, listenAll, checkStyle, OPEN, CLOSE };
+export { grab, grabAll, render, onLoad, onLoadAll, onClick, onClickAll, listen, listenAll, stopListen, stopListenAll, checkStyle, OPEN, CLOSE };
