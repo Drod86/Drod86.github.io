e2154c5a1cb98674a6bc0f6fa37eaccb4ad3abab
Daniel Rodriguez <daniel.r.rodriguez86@gmail.com>
2023-07-28 00:37:44 -0500

    Added form validation error message styles and functionality for it to disappear when you click away

--- src/components/popups.mjs
+++ src/components/popups.mjs
@@ -1,15 +1,28 @@
-import {node, grab, render, onClick} from '../utils/utils.mjs';
+import {node, grab, render, onClick, stopListen} from '../utils/utils.mjs';
+const container = grab('.container');
+const listeners = []
+
+const closePopup = (parentNode, e) => {
+  let popup = parentNode.lastChild;
+  if (e.target !== popup) {
+    parentNode.removeChild(popup);
+    stopListen(container, 'click', listeners[0]);
+  }
+}
 
 const fadingPopup = (parentNode,className, text) => {
   let popup = node('div');
-  popup.className = `${className} fadeOut`;
+  popup.className = `${className}`;
   parentNode.appendChild(popup);
   let errorMsgEl = grab(`.${className}`);
   render(errorMsgEl, text);
-  onClick(grab('.container'), (e) => {
-    let isVisible = parentNode.lastChild.className;
-    if (isVisible && errorMsgEl && e.target.className !== errorMsgEl.className) parentNode.removeChild(errorMsgEl);
-  });
+
+  setTimeout(() => {
+    errorMsgEl.classList.add('fadeOut');
+  }, 4000);
+
+  listeners.push(closePopup.bind(this, parentNode));
+  onClick(container, listeners[0]);
 }
 
 export default fadingPopup;
